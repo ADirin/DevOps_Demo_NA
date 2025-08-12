a4fbff54a9573d8fe559a7eb700fac235fa81b4b
amirdi <amir.dirin@yahoo.com>
2025-08-12 09:53:27 +0300

    second commit, docker added

--- src/main/java/Myapp.java
+++ src/main/java/Myapp.java
@@ -1,15 +1,28 @@
+//public class Myapp {
+//
+//    public int addMe(int a, int b) {
+//        return a + b;
+//    }
+//    public int subMe(int a, int b) {
+//        return a - b;
+//    }
+//    public int mulMe(int a, int b) {
+//        return a * b;
+//    }
+//    public int divMe(int a, int b) {
+//        return a / b;
+//    }
+//
+//
+//}
 public class Myapp {
+    public static void main(String[] args) {
+        Myapp app = new Myapp();
+        System.out.println("2 + 3 = " + app.addMe(2, 3));
+    }
 
-    public int addMe(int a, int b) {
-        return a + b;
-    }
-    public int subMe(int a, int b) {
-        return a - b;
-    }
-    public int mulMe(int a, int b) {
-        return a * b;
-    }
-    public int divMe(int a, int b) {
-        return a / b;
-    }
+    public int addMe(int a, int b) { return a + b; }
+    public int subMe(int a, int b) { return a - b; }
+    public int mulMe(int a, int b) { return a * b; }
+    public int divMe(int a, int b) { return a / b; }
 }
