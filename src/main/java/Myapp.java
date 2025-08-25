6598b557e32448c14f4e75eebd40892551bae3c8
amirdi <amir.dirin@yahoo.com>
2025-08-25 13:02:01 +0300

    fifth commit, jenkinsfile ok, promot docker run -it id

--- src/main/java/Myapp.java
+++ src/main/java/Myapp.java
@@ -1,24 +1,16 @@
-//public class Myapp {
-//
-//    public int addMe(int a, int b) {
-//        return a + b;
-//    }
-//    public int subMe(int a, int b) {
-//        return a - b;
-//    }
-//    public int mulMe(int a, int b) {
-//        return a * b;
-//    }
-//    public int divMe(int a, int b) {
-//        return a / b;
-//    }
-//
-//
-//}
+import java.util.Scanner;
+
 public class Myapp {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Myapp app = new Myapp();
-        System.out.println("2 + 3 = " + app.addMe(2, 3));
+        System.out.println("Enter the first number");
+        int num1 = scanner.nextInt();
+        System.out.println("Enter the second number");
+        int num2 = scanner.nextInt();
+
+        System.out.println("2 + 3 = " + app.addMe(num1, num2));
     }
 
     public int addMe(int a, int b) { return a + b; }
